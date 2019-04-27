225ca7374d97bbd6a711b09aadcd5d73a5eabbfa
gavinoconnor <gedmund@gmail.com>
2019-04-27 19:06:40 -0400

    Done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
+  require 'pry'
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  # new_array = []
+  array.group_by{|item|item}.map do |key, value|
+    new_hash[key] = value.count
+    # new_array << [key => value.count]
+    # new_hash << new_array.to_h
+  end
+  new_hash
 end
- 
